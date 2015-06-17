a7641ee5b84ff7a6001c228bb6bc8feeb522b8fe
rkrabek <rkrabek@gmail.com>
2015-06-17 09:25:12 +0200

    updated leap.rb to include rule for 400's

--- RobertKrabek-rkrabek/d2/leap.rb
+++ RobertKrabek-rkrabek/d2/leap.rb
@@ -2,7 +2,7 @@ puts "give me a start year and then an end year"
 startyear = gets.chomp.to_i
 endyear = gets.chomp.to_i
 for i in startyear..endyear
-	if (i % 4 == 0) && (i % 100 != 0)
+	if (i % 4 == 0) && ((i % 100 != 0) || (i % 400 == 0))
 		puts "#{i}\n"
 		i += 1
 	else
