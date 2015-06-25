1e826e945fc9508a61c8d2ef0225922a70690e37
rkrabek <rkrabek@gmail.com>
2015-06-25 21:41:56 +0200

    created associations in cheetah_watch

--- RobertKrabek-rkrabek/d8/cheetah_watch/db/schema.rb
+++ RobertKrabek-rkrabek/d8/cheetah_watch/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625134420) do
+ActiveRecord::Schema.define(version: 20150625164314) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150625134420) do
     t.boolean  "rideable"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.integer  "shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
