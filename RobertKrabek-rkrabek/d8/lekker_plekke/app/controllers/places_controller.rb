222a5ff4239c794015dea096579f27df41c038cf
rkrabek <rkrabek@gmail.com>
2015-06-25 09:44:38 +0200

    completed edit, update, and destroy actions for lekker plekke

--- RobertKrabek-rkrabek/d8/lekker_plekke/app/controllers/places_controller.rb
+++ RobertKrabek-rkrabek/d8/lekker_plekke/app/controllers/places_controller.rb
@@ -11,6 +11,10 @@ class PlacesController < ApplicationController
 		@place = Place.new
 	end
 
+	def edit
+		@place = Place.find params[:id]
+	end
+
 	def create
 		@place = Place.new(place_params)
 
@@ -19,6 +23,23 @@ class PlacesController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def update
+		@place = Place.find params[:id]
+
+		if @place.update(place_params)
+			redirect_to @place
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@place = Place.find params[:id]
+		@place.destroy
+
+		redirect_to places_path
 	end
 
 	private
