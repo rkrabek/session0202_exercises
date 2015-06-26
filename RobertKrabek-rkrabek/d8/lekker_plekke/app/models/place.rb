2413a97f0eae734f46c5799ec3a958d9d93125e6
rkrabek <rkrabek@gmail.com>
2015-06-26 15:40:12 +0200

    added comments functionality to cool places

--- RobertKrabek-rkrabek/d8/lekker_plekke/app/models/place.rb
+++ RobertKrabek-rkrabek/d8/lekker_plekke/app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :name, :description, :location, presence: true
 end
--- RobertKrabek-rkrabek/d8/lekker_plekke/config/routes.rb
+++ RobertKrabek-rkrabek/d8/lekker_plekke/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :places
+  resources :places do
+    resources :comments
+  end
   root 'places#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
